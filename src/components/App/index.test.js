9bab846dc61e9c9e0cb82a61e01daceb0154e81d
Gloria <hanning_hit@hotmail.com>
2022-07-27 21:38:41 +1000

    update test

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -2,11 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 test("renders learn react link", () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  render(<App />);
   const linkElement = screen.getByText("Flickr Search");
   expect(linkElement).toBeInTheDocument();
 });
