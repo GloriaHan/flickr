188abc6d214729d94c19500e3706cf2f2ea04971
Gloria <hanning_hit@hotmail.com>
2022-07-27 21:44:51 +1000

    eslint

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -4,30 +4,28 @@ import queryString from "query-string";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
-import ListSubheader from "@mui/material/ListSubheader";
 
 const FLICKR_API_KEY = process.env.REACT_APP_FLICKR_API_KEY;
 
 export default function SearchResults() {
   const params = useParams();
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
   const [resultData, setResultData] = useState("");
   // const { serverId, photoId, secret } = resultData;
 
-  const fetchFlickr = async () => {
-    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&text=${params.queryText}&format=json&nojsoncallback=1`;
-    setLoading(true);
-    const res = await fetch(url);
-    const data = await res.json();
-    setLoading(false);
-    return data;
-  };
-
   useEffect(() => {
+    const fetchFlickr = async () => {
+      const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&text=${params.queryText}&format=json&nojsoncallback=1`;
+      // setLoading(true);
+      const res = await fetch(url);
+      const data = await res.json();
+      // setLoading(false);
+      return data;
+    };
+
     fetchFlickr().then((response) => {
       setResultData(response);
     });
-    console.log(resultData);
   }, [params.queryText]);
 
   return (
@@ -38,7 +36,7 @@ export default function SearchResults() {
         {resultData &&
           resultData.photos.photo.map((item) => (
             <Link
-             to={`/photo/${item.id}?${queryString.stringify({
+              to={`/photo/${item.id}?${queryString.stringify({
                 title: item.title,
                 server: item.server,
                 secret: item.secret,
